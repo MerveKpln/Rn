1cd46c1d2a16613d3673413ac3d0df6bd0869866
MerveKpln <hamuss001@gmail.com>
2023-07-11 15:16:04 +0400

    edited

--- Drawer_Navigation/App.js
+++ Drawer_Navigation/App.js
@@ -1,53 +1,102 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
 import 'react-native-gesture-handler';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import {  createDrawerNavigator } from '@react-navigation/drawer';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useDrawerStatus } from '@react-navigation/drawer';
+import {
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
 const Drawer = createDrawerNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      
-      
-    </View>
-);}
-
-  function UserScreen({ navigation }){
-    console.log("Navigasyon", navigation);
-
-    return (<View style={{
-      flex:1,
-      justifyContent:'center',
-      alignItems:"center"
-    }}>
-      <Text>User Screen  </Text>
-    <TextInput style={{height:40, width:"100%", borderWidth:2}}/>
-    </View>
-  )}
-
-function OtherScreen({  navigation }){
-
-  return (<View style={{
-    flex:1, 
-    justifyContent:'center',
-    alignItems:"center"
-  }}>
-    <Text>Other Screen </Text>
-    
-  </View>
-);}
-
-const App =()=>{
+function CustomDrawerContent({state, descriptors, navigation}){
   return(
-   <NavigationContainer>
-    <Drawer.Navigator  initialRouteName='Home' defaultStatus="open">
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="User" component={UserScreen} />
-      <Drawer.Screen name="Other" component={OtherScreen} />
-    </Drawer.Navigator>
+    <DrawerContentScrollView>
+      <View style={{height:300, backgroundColor:"red",alignItems:'center', justifyContent:'center'}} >
+<Text>Loogo </Text>
+      </View>
+      {
+      state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
 
-   </NavigationContainer>
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          navigation.navigate(route.name)
+        }
+return(
+  <DrawerItem 
+  key={index}
+  activeTintColor='red' 
+  inactiveTintColor='blue' 
+  focused={isFocused} 
+  activeBackgroundColor='white' 
+  inactiveBackgroundColor='green'
+  onPress={onPress}
+  label={ ({ focused, color }) => <Text style={{ color }}>{focused ? 'Focused text' : label}</Text>}/>
+)
+
+      })}
+    
+    </DrawerContentScrollView>
   )
+}
+
+function Home({navigation}){
+  const isDrawerOpen = useDrawerStatus() === 'open';
+  return(
+    <View>
+      <TouchableOpacity onPress={()=>{
+        navigation.toggleDrawer();
+      }}>
+        <Text>{isDrawerOpen ? "Open" : "Close"}</Text>
+        <Text >
+          toogle a menu
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+function Feed(){
+  return(
+    <Text style={{alignItems:'center',justifyContent:"center"}}>Hello Feed</Text>
+  )
+}
+function Article(){
+  return(
+    <Text>Hello Article</Text>
+  )
+}
+const App=()=> {
+  return (
+    <NavigationContainer>
+       <Drawer.Navigator initialRouteName='Home' screenOptions={{
+        drawerStyle: {
+          backgroundColor: 'pink',
+          width: 240,
+        },
+        drawerPosition:'right',
+        drawerType: 'slide',
+        overlayColor:'#c6cbef'
+      }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+ 
+    
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 export default App;
