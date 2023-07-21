ec15adbd848500e453084dca340686f629580e84
MerveKpln <hamuss001@gmail.com>
2023-07-21 19:17:22 +0400

    changed

--- Drawer_Stack_Tab/App.js
+++ Drawer_Stack_Tab/App.js
@@ -5,18 +5,21 @@ import {NavigationContainer} from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Drawer =createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const StackOne=()=>{
+const StackOne=({navigation})=>{
   return(
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+    <View style={styles.container}>
       <Text>
         Page 1
       </Text>
+      <Button title='Sayfa 2 ye git' onPress={()=>{
+        navigation.navigate("Page2")
+      }} />
 
     </View> 
   )
@@ -24,7 +27,7 @@ const StackOne=()=>{
 }
 const StackTwo=()=>{
   return(
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+    <View style={[styles.container,styles.text]}>
       <Text>
         Page 2
       </Text>
@@ -35,7 +38,7 @@ const StackTwo=()=>{
 }
 const StackThree=()=>{
   return(
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+    <View style={styles.container}>
       <Text>
         Page 3
       </Text>
@@ -46,8 +49,8 @@ const StackThree=()=>{
 }
 const StackFour=()=>{
   return(
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
         Page 4
       </Text>
 
@@ -99,4 +102,17 @@ const App=()=>{
     </NavigationContainer>
   )
 }
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  text:{
+    backgroundColor:"pink",
+   
+  }
+})
+
 export default App;
